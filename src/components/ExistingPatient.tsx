f7cd8ac06298b688b516aeb2b482c85563799eea
Chandima Ranaweera <chan4lk@gmail.com>
2024-05-12 07:33:58 +0530

    refactor and add back button

--- src/components/ExistingPatient.tsx
+++ src/components/ExistingPatient.tsx
@@ -1,29 +1,18 @@
 import { Formik } from "formik";
-import * as Yup from "yup";
 import { SimpleInput } from "../components/SimpleInput";
 import { SimpleSelect } from "../components/SimpleSelect";
+import { validationSchema } from "./Schema";
+import { Back } from "./BackButton";
 
-const validationSchema = Yup.object().shape({
-  fullname: Yup.string()
-    .min(2, "Too Short!")
-    .max(50, "Too Long!")
-    .required("Required"),
-  mobile: Yup.string()
-    .matches(/^[0-9]{10}$/, "Invalid phone number")
-    .required("Required"),
-  treatment_type: Yup.string().required("Required"),
-  total_amount: Yup.number().positive().required("Required"),
-  paid_amount: Yup.number()
-    .min(0, "Amount must be positive")
-    .max(Yup.ref("total_amount"), "Amount must not exceed the total amount")
-    .required("Required"),
-});
+export const ExistingPatient = () => { 
+  
 
-const NewPatient = () => {
-  return <div className="container mx-auto">
-    <div className="flex items-center justify-center">
-      <h1 className="text-3xl font-bold mb-8">Add New Patient!</h1>
-      </div>
+  return (
+  <div className="container mx-auto">
+    <div className="flex items-center ">
+      <Back />
+      <h1 className="text-3xl font-bold mt-8 mb-8 px-5 py-2">Existing Patient</h1>
+    </div>
     <Formik
       initialValues={{
         fullname: "",
@@ -31,20 +20,14 @@ const NewPatient = () => {
         treatment_type: "",
         total_amount: "",
         paid_amount: "",
+        previous_paid: "0",
       }}
       validationSchema={validationSchema}
-      onSubmit={async (values, { resetForm }) => {
-        const insert = await window.electronAPI.insertPatient({
-          ...values,
-          total_amount: parseFloat(values.total_amount),
-          paid_amount: parseFloat(values.paid_amount)
-        });
-
-        console.log('Insert: ');
-        console.table(insert);
-        console.log('Fetch: ');
-        console.table(await window.electronAPI.fetchAll());
-        resetForm();
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
       }}
     >
       {({
@@ -100,19 +83,40 @@ const NewPatient = () => {
               field="paid_amount"
             />
           </div>
-          <div className="flex items-center">
-            <label
-              htmlFor="amount_due"
-              className="block text-sm font-medium text-gray-700 mr-4"
-            >
-              Amount Due
-            </label>
-            <span
-              id="amount_due"
-              className="text-lg font-semibold text-gray-900"
-            >
-              ${(parseFloat(values.total_amount || '0') - parseFloat(values.paid_amount || '0')).toFixed(2)}
-            </span>
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <label
+                htmlFor="amount_due"
+                className="block text-sm font-medium text-gray-700 mr-4"
+              >
+                Previous Payments
+              </label>
+              <span
+                id="amount_due"
+                className="text-lg font-semibold text-gray-900"
+              >
+                ${parseFloat(values.previous_paid || "0").toFixed(2)}
+              </span>
+            </div>
+            <div className="flex items-center">
+              <label
+                htmlFor="amount_due"
+                className="block text-sm font-medium text-gray-700 mr-4"
+              >
+                Amount Due
+              </label>
+              <span
+                id="amount_due"
+                className="text-lg font-semibold text-gray-900"
+              >
+                $
+                {(
+                  parseFloat(values.total_amount || "0") -
+                  parseFloat(values.paid_amount || "0") -
+                  parseFloat(values.previous_paid || "0")
+                ).toFixed(2)}
+              </span>
+            </div>
           </div>
           <div>
             <button
@@ -127,6 +131,5 @@ const NewPatient = () => {
       )}
     </Formik>
   </div>
-};
-
-export default NewPatient;
+);
+}
