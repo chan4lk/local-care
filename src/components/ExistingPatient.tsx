92cf2ca9b86a8035d14a9712dc62a10511f7ef23
Thilan Buddhika <116415642+MuhandiramTB@users.noreply.github.com>
2024-05-29 11:44:23 +0530

    New

--- src/components/ExistingPatient.tsx
+++ src/components/ExistingPatient.tsx
@@ -46,6 +46,14 @@ export const ExistingPatient = () => {
 
   const handlePrint = useReactToPrint({
     content: () => printRef.current,
+    trigger: () => (
+      <button
+        type="button"
+        className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo"
+      >
+        Print Bill
+      </button>
+    ),
   });
 
   return (
