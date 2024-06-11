114652292c7f3d00ad0c1e1624e740b709429b25
Thilan Buddhika <116415642+MuhandiramTB@users.noreply.github.com>
2024-06-11 16:48:18 +0530

    BillFormat More User Friendly design

--- src/components/BillFormat.tsx
+++ src/components/BillFormat.tsx
@@ -28,61 +28,63 @@ const BillFormat = React.forwardRef<HTMLDivElement, BillFormatProps>(
         className="p-2 text-xs"
         style={{ width: '12cm', height: '10.1cm', marginTop: '3cm', marginLeft: '0.2cm', marginRight: '0.2cm' }}
       >
-        <div className="border-dashed border-2 border-gray-200 p-2">
-          <div>
-            <div className="flex">
+        <div className="border-dashed border-2 border-gray-200 p-2 rounded-md">
+          <div className="mb-2">
+            <div className="flex mb-1">
               <p className="font-bold w-2/5">BILL NO</p>
               <p className="w-3/5">: {patient.referenceNumber || values.referenceNumber}</p>
             </div>
-            <div className="flex">
+            <div className="flex mb-1">
               <p className="font-bold w-2/5">BILL DATE</p>
               <p className="w-3/5">: {new Date().toLocaleString()}</p>
             </div>
-            <div className="flex">
+            <div className="flex mb-1">
               <p className="font-bold w-2/5">PATIENT</p>
               <p className="w-3/5">: {patient.fullname}</p>
             </div>
-            <div className="flex">
+            <div className="flex mb-1">
               <p className="font-bold w-2/5">TELL NO</p>
               <p className="w-3/5">: {patient.mobile}</p>
             </div>
           </div>
 
-          <table className="min-w-full border-dashed border-t border-gray-800">
-            <thead className="bg-white-">
+          <table className="min-w-full border-collapse text-xs">
+            <thead className="bg-gray-100">
               <tr>
-                <th className="text-center p-1 border-dashed border-t border-b border-gray-300">ITEM</th>
-                <th className="text-center p-1 border-dashed border-t border-b border-gray-300">QTY</th>
-                <th className="text-right p-1 border-dashed border-t border-b border-gray-300">AMOUNT</th>
+                <th className="text-left p-1 border-dashed border-b border-gray-300">ITEM</th>
+                <th className="text-left p-1 border-dashed border-b border-gray-300">QTY</th>
+                <th className="text-right p-1 border-dashed border-b border-gray-300">AMOUNT</th>
               </tr>
             </thead>
             <tbody className="bg-white">
               <tr>
-                <td className="text-center px-6 p-0 border-dashed border-b border-gray-300">{values.treatment}</td>
-                <td className="text-center px-6 p-0 border-dashed border-b border-gray-300">1</td>
-                <td className="text-right px-6 p-0 border-dashed border-b border-gray-300">
+                <td className="text-left px-1 py-1 border-dashed border-b border-gray-300 whitespace-nowrap">
+                  {values.treatment}
+                </td>
+                <td className="text-left px-1 py-1 border-dashed border-b border-gray-300">1</td>
+                <td className="text-right px-1 py-1 border-dashed border-b border-gray-300">
                   {formatNumberWithCommas(parseFloat(values.total_amount || '0'))}
                 </td>
               </tr>
               <tr>
-                <td className="px-6 p-0"></td>
-                <td className="px-6 font-bold p-0">TOTAL (LKR)</td>
-                <td className="px-6 text-right p-0">{formatNumberWithCommas(parseFloat(values.total_amount || '0'))}</td>
+                <td className="px-1 p-0"></td>
+                <td className="px-1 p-0 font-bold">TOTAL (LKR)</td>
+                <td className="px-1 p-0 text-right">{formatNumberWithCommas(parseFloat(values.total_amount || '0'))}</td>
               </tr>
               <tr>
-                <td className="px-6 p-0 border-dashed border-t border-white"></td>
-                <td className="px-6 font-bold p-0 border-dashed border-t border-white">PREVIOUS PAYMENTS</td>
-                <td className="px-6 text-right p-0 border-dashed border-t border-white">{formatNumberWithCommas(parseFloat(values.previous_paid || '0'))}</td>
+                <td className="px-1 p-0 border-dashed border-t border-white"></td>
+                <td className="px-1 p-0 font-bold border-dashed border-t border-white whitespace-nowrap">PREVIOUS PAYMENTS</td>
+                <td className="px-1 p-0 text-right border-dashed border-t border-white">{formatNumberWithCommas(parseFloat(values.previous_paid || '0'))}</td>
               </tr>
               <tr>
-                <td className="px-6 p-0 border-dashed border-t border-white"></td>
-                <td className="px-6 font-bold p-0 border-dashed border-t border-white">PAID AMOUNT</td>
-                <td className="px-6 text-right p-0 border-dashed border-t border-white">{formatNumberWithCommas(parseFloat(values.paid_amount || '0'))}</td>
+                <td className="px-1 p-0 border-dashed border-t border-white"></td>
+                <td className="px-1 p-0 font-bold border-dashed border-t border-white">PAID AMOUNT</td>
+                <td className="px-1 p-0 text-right border-dashed border-t border-white">{formatNumberWithCommas(parseFloat(values.paid_amount || '0'))}</td>
               </tr>
               <tr>
-                <td className="px-6 p-0 border-dashed border-t border-white"></td>
-                <td className="px-6 font-bold p-0 border-dashed border-t border-white">DUE AMOUNT</td>
-                <td className="px-6 text-right p-0 border-dashed border-t border-white">
+                <td className="px-1 p-0 border-dashed border-t border-white"></td>
+                <td className="px-1 p-0 font-bold border-dashed border-t border-white">DUE AMOUNT</td>
+                <td className="px-1 p-0 text-right border-dashed border-t border-white">
                   {formatNumberWithCommas(
                     parseFloat(values.total_amount || '0') -
                     parseFloat(values.previous_paid || '0') -
